b0d04b664ace3eb7450230bbcee37fd2d300e087
Gorka <gorkacarmonapino@gmail.com>
2025-01-26 22:55:54 +0100

    Actualizacion 0.13

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,29 +1,7 @@
-document.querySelectorAll('a[href^="#"]').forEach(anchor => {
-    anchor.addEventListener('click', function (e) {
-        e.preventDefault();
-        document.querySelector(this.getAttribute('href')).scrollIntoView({
-            behavior: 'smooth'
-        });
-    });
-});
 
 
-// Seleccionamos el botón y el menú
-const menuToggle = document.querySelector('.navbar-toggler');
-const navbarNav = document.querySelector('#navbarNav');
 
-// Añadimos el evento click para mostrar/ocultar el menú
-menuToggle.addEventListener('click', () => {
-    navbarNav.classList.toggle('collapse');
-    navbarNav.classList.toggle('show');
-});
 
-// Cerrar el menú si se hace clic fuera del mismo (opcional)
-document.addEventListener('click', (event) => {
-    if (!navbarNav.contains(event.target) && !menuToggle.contains(event.target)) {
-        navbarNav.classList.remove('show');
-    }
-});
 
 
 
